26aeb80dfcdb8b7904dd3c2ad34ba36476a97562
ryan <ryan@ryandeMacBook-Air.local>
2022-05-11 17:04:08 -0700

    new start on front end

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Test from "./pages/test"
+import Homepage from "./pages/homepage"
+import Profile from "./pages/profile"
+import Registration from "./pages/registration"
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/test' element={<Test />}></Route>
+          <Route path='' element={<Homepage/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          <Route path='/registration' element={<Registration/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
