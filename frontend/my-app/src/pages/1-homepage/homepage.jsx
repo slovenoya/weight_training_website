086aaa2433abb2e1ace4e3c8da56d1f9ae8600d4
slovenoya <ryanzhang880@gmail.com>
2022-09-30 11:59:02 -0700

    learn more(why us) section first draft finished

--- frontend/my-app/src/pages/1-homepage/homepage.jsx
+++ frontend/my-app/src/pages/1-homepage/homepage.jsx
@@ -46,39 +46,47 @@ const HomePage = () => {
             <div className='why-us-item why-us-progressive'>
               <img src={require('./images/plate-stack.jpg')} alt="stacked plates" />
               <div className='why-us-item-text'>
-                <span className='why-us-item-slogan'>Easy, acurate, and </span>
-                <span className='why-us-item-description'></span>
+                <span className='why-us-item-slogan'>Easy Overloading</span>
+                <span className='why-us-item-description'>Gymcat planner will overload 
+                your training weight linearly or periodically according to your 
+                customized plan, and will result in your making real and reasonable progress 
+                through your workout. </span>
               </div>
             </div>
 
             <div className='why-us-item why-us-customize'>
               <img src={require('./images/measure-arm.jpg')} alt="measuring bicepts" />
               <div className='why-us-item-text'>
-                <span className='why-us-item-slogan'></span>
-                <span className='why-us-item-description'></span>
+                <span className='why-us-item-slogan'>Personalized Training</span>
+                <span className='why-us-item-description'>No more look up online for hours 
+                just to make a plan, just measure a few numbers that Gymcat needs and we will make 
+                your own workout plan according to your current level. </span>
               </div>
             </div>
-
             <div className='why-us-item why-us-classic'>
               <img src={require('./images/greek-god.jpg')} alt="greek god statue" />
               <div className='why-us-item-text'>
-                <span className='why-us-item-slogan'></span>
-                <span className='why-us-item-description'></span>
+                <span className='why-us-item-slogan'>Classic Plan</span>
+                <span className='why-us-item-description'>Famous trainer's plan are displayed and 
+                implemented in here, and we will customize the training weight for you. 5*5, Smolov, 
+                Jacked and Tan, etc. </span>
               </div>
             </div>
 
             <div className='why-us-item why-us-form'>
               <img src={require('./images/form-watch.jpg')} alt="watchout for form" />
               <div className='why-us-item-text'>
-                <span className='why-us-item-slogan'></span>
-                <span className='why-us-item-description'></span>
+                <span className='why-us-item-slogan'>Form Tutorial</span>
+                <span className='why-us-item-description'>Compiled form tutorials are provided
+                under different workout sets, we also provide web crawlers to find the most viewed
+                tutorials of each workout movements for you, no need to switch between websites</span>
               </div>
             </div>
           </div>
         </section>
 
         <section className='section-for-beginners' id='beginner'>
-
+          <div></div>
         </section>
 
         <section className='section-for-experienced' id='experienced'>
