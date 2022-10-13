61448404574ed9ddd20a3663d7c9927667173a6e
slovenoya <ryanzhang880@gmail.com>
2022-10-13 09:31:07 -0700

    update

--- frontend/my-app/src/pages/2-loggin/login.jsx
+++ frontend/my-app/src/pages/2-loggin/login.jsx
@@ -1,9 +1,33 @@
 import {React} from 'react'
 
+import './login.css';
+
 const Login = () => {
   return (
-    <div>
-      
+    <div className='login-page'>
+      <section className='login-section'>
+        <div className='login-head'>
+          <p>Login</p>
+          <p>Start Your Change Today!</p>
+        </div>
+
+        <div className='login-body'>
+          <div className='login-input login-email'>
+            <p>Email</p>
+            <input type="text" />
+          </div>
+          <div className='login-input login-password'>
+            <p>Password</p>
+            <input type="password" />
+          </div>
+          <div className='login-btn'>
+            <button>Login</button>
+          </div>
+        </div>
+      </section>
+      <section className='image-section'>
+        <img src={require('./images/plate-stack.jpg')} alt="barbell" />
+      </section>
     </div>
   )
 }
