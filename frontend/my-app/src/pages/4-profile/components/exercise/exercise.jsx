805b55897ea23d246e9721c29c08f18ae75cd289
slovenoya <ryanzhang880@gmail.com>
2022-11-08 09:35:15 -0800

    css on exercise

--- frontend/my-app/src/pages/4-profile/components/exercise/exercise.jsx
+++ frontend/my-app/src/pages/4-profile/components/exercise/exercise.jsx
@@ -7,9 +7,9 @@ const Exercise = () => {
   const sample1={id:1, name:'Bench Press', description:'push harder!', type:'chest', video:null}
   const sample2={id:2, name:'Squat', description:'Light weight baby', type:'leg', video:null}
   const sample3={id:3, name:'pull up', description:'darken the sky', type:'back', video:null}
-  const sample4={id:4, name:'pull up', description:'darken the sky', type:'back', video:null}
-  const sample5={id:5, name:'pull up', description:'darken the sky', type:'back', video:null}
-  const sample6={id:6, name:'pull up', description:'darken the sky', type:'back', video:null}
+  const sample4={id:4, name:'push up', description:'better chest ', type:'chest', video:null}
+  const sample5={id:5, name:'lat pull down', description:'side of your lat', type:'back', video:null}
+  const sample6={id:6, name:'deadlift', description:'strong leg', type:'leg', video:null}
   const exercises = [sample1, sample2, sample3, sample4, sample5, sample6]
   return (
     <div className='page-container'>
--- frontend/my-app/src/pages/4-profile/components/exercise/exerciseProto.jsx
+++ frontend/my-app/src/pages/4-profile/components/exercise/exerciseProto.jsx
@@ -1,9 +1,10 @@
 import {React} from 'react'
+import './exerciseProto.jsx'
 import './exercise-proto.css'
 
-
 const ExerciseProto = (exercise) => {
-  return (<div>
+  return (
+  <div className='exercise-item'>
     <p>{exercise.name}</p>
     <p>{exercise.description}</p>
     <p>{exercise.type}</p>
