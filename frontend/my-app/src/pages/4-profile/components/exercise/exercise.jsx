088066fb7410f9638789f307b649c4167a32036e
slovenoya <ryanzhang880@gmail.com>
2022-11-09 22:41:02 -0800

    exercise page update

--- frontend/my-app/src/pages/4-profile/components/exercise/exercise.jsx
+++ frontend/my-app/src/pages/4-profile/components/exercise/exercise.jsx
@@ -1,18 +1,59 @@
-import {React} from 'react'
+import {React, useState, useEffect} from 'react'
 import ExerciseProto from './exerciseProto/exerciseProto'
 import exercises from './exerciseInfo.json'
 import './exercise.css'
 
+const DEFAULT = 1;
+const DESCRIPTION = 2;
+
+const ExerciseDescription = props => {
+  const exercise = props.exercise;
+  const handleClick = () => {
+    props.setPageType(DEFAULT);
+  }
+  return (
+    <div>
+      <p className='description-back' onClick={handleClick}>go back</p>
+      <p>{exercise.name}</p>
+      <img src={exercise.url} alt="exercise" width='100px'/>
+      <p>{exercise.description}</p>
+    </div>
+  )
+}
+
+const ExerciseList = props => {
+  const handleClick = (exercise) => {
+    props.setExercise(exercise);
+    props.setPageType(DESCRIPTION)
+  }
+
+  return (
+    <div className='exercise-container'>
+      {exercises.map((exercise) => (
+        <div key={exercise.id} onClick={() => {handleClick(exercise)}}>
+          {ExerciseProto(exercise)}
+        </div>
+      ))}
+    </div>
+  )
+}
+
 const Exercise = () => {
+  const [page, setPage] = useState()
+  const [pageType, setPageType] = useState(DEFAULT);
+  const [exercise, setExercise] = useState();
+
+  useEffect(()=>{
+    if (pageType === DEFAULT) {
+      setPage(<ExerciseList setExercise={setExercise} setPageType={setPageType}></ExerciseList>)
+    } else if (pageType === DESCRIPTION) {
+      setPage(<ExerciseDescription exercise={exercise} setPageType={setPageType}></ExerciseDescription>)
+    }
+  }, [pageType, exercise])
+
   return (
     <div className='page-container'>
-      <div className='exercise-container'>
-        {exercises.map((exercise) => (
-          <div key={exercise.id}>
-            {ExerciseProto(exercise)}
-          </div>
-        ))}
-      </div>
+      {page}
     </div>
 
   )
